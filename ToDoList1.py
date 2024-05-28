c6826613da815c6658bce5ce4fc371c18894ef29
VannAdp <85007517+ChinChinMarux@users.noreply.github.com>
2024-05-28 23:03:56 +0700

    +background

--- ToDoList1.py
+++ ToDoList1.py
@@ -1,6 +1,7 @@
 import json
 import tkinter as tk
 from tkinter import simpledialog
+from PIL import Image, ImageTk
 
 class TaskManagerGUI:
     def __init__(self, root):
@@ -9,27 +10,32 @@ class TaskManagerGUI:
         self.root.geometry("685x390")
         self.root.resizable(False, False)
 
+        self.background_image = Image.open("stl.jpg") 
+        self.background_photo = ImageTk.PhotoImage(self.background_image)
+        self.background_label = tk.Label(root, image=self.background_photo)
+        self.background_label.place(relwidth=1, relheight=1)
+
         self.tasks = self.load_tasks()
 
         self.create_widgets()
 
     def create_widgets(self):
-        self.task_label = tk.Label(pady = 2, text="List Tugas Kamu Hari Ini :)", font =(15))
+        self.task_label = tk.Label(self.root, pady=2, text="List Tugas Kamu Hari Ini :)", font=(15), bg="white", highlightthickness=0)
         self.task_label.pack()
         self.task_listbox = tk.Listbox(self.root, width=50, height=15)
         self.task_listbox.pack(pady=5)
 
         self.refresh_tasks()
 
-        add_button = tk.Button(self.root, text="Tambah Tugas", command=self.add_task)
+        add_button = tk.Button(self.root, text="Tambah Tugas", command=self.add_task, bg="white", highlightthickness=0)
         add_button.pack(pady=5)
 
-        done_button = tk.Button(self.root, text="Tandai Selesai", command=self.mark_done)
+        done_button = tk.Button(self.root, text="Tandai Selesai", command=self.mark_done, bg="white", highlightthickness=0)
         done_button.pack(pady=5)
 
-        delete_button = tk.Button(self.root, text="Hapus Tugas", command=self.delete_task)
-        delete_button.pack(pady=5) 
-        
+        delete_button = tk.Button(self.root, text="Hapus Tugas", command=self.delete_task, bg="white", highlightthickness=0)
+        delete_button.pack(pady=5)
+
     def load_tasks(self):
         try:
             with open("tugas.json", "r") as file:
@@ -43,7 +49,7 @@ class TaskManagerGUI:
             json.dump(self.tasks, file)
 
     def refresh_tasks(self):
-        self.task_listbox.delete(0, tk.END) 
+        self.task_listbox.delete(0, tk.END)
         for idx, task in enumerate(self.tasks, 1):
             status = "Selesai" if task['done'] else "Belum Selesai"
             self.task_listbox.insert(tk.END, f"{idx}. {task['description']} - {status}")
